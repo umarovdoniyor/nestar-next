1a7fcce09808c7da623e609a0cf5e7c832fbfd54
Danny <mr.danny951008@gmail.com>
2026-02-06 02:22:39 +0900

    fix: develop CommunityBoards article components

--- libs/components/homepage/CommunityBoards.tsx
+++ libs/components/homepage/CommunityBoards.tsx
@@ -4,6 +4,10 @@ import useDeviceDetect from '../../hooks/useDeviceDetect';
 import { Stack, Typography } from '@mui/material';
 import CommunityCard from './CommunityCard';
 import { BoardArticle } from '../../types/board-article/board-article';
+import { GET_BOARD_ARTICLES } from '../../../apollo/user/query';
+import { useQuery } from '@apollo/client';
+import { BoardArticleCategory } from '../../enums/board-article.enum';
+import { T } from '../../types/common';
 
 const CommunityBoards = () => {
 	const device = useDeviceDetect();
@@ -16,6 +20,44 @@ const CommunityBoards = () => {
 	const [freeArticles, setFreeArticles] = useState<BoardArticle[]>([]);
 
 	/** APOLLO REQUESTS **/
+	// News Articles
+	const {
+		loading: getNewsArticlesLoading,
+		error: getNewsArticlesError,
+		data: getNewsArticlesData,
+		refetch: getNewsArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only',
+		variables: {
+			input: { ...searchCommunity, limit: 6, search: { articleCategory: BoardArticleCategory.NEWS } },
+		},
+		notifyOnNetworkStatusChange: true,
+		onError: (error) => {
+			console.error('Error fetching news articles:', error);
+		},
+		onCompleted: (data: T) => {
+			setNewsArticles(data?.getBoardArticles?.list);
+		},
+	});
+	// Free Articles
+	const {
+		loading: getFreeArticlesLoading,
+		error: getFreeArticlesError,
+		data: getFreeArticlesData,
+		refetch: getFreeArticlesRefetch,
+	} = useQuery(GET_BOARD_ARTICLES, {
+		fetchPolicy: 'network-only',
+		variables: {
+			input: { ...searchCommunity, limit: 3, search: { articleCategory: BoardArticleCategory.FREE } },
+		},
+		notifyOnNetworkStatusChange: true,
+		onError: (error) => {
+			console.error('Error fetching free articles:', error);
+		},
+		onCompleted: (data: T) => {
+			setFreeArticles(data?.getBoardArticles?.list);
+		},
+	});
 
 	if (device === 'mobile') {
 		return <div>COMMUNITY BOARDS (MOBILE)</div>;
