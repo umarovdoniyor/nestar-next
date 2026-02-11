0e23f4c2c843f83f7875038800fcdba8e86dab30
Danny <mr.danny951008@gmail.com>
2026-02-12 03:56:34 +0900

    feat: develop MyFavorites && RecentlyVisited components business logic

--- libs/components/mypage/MyProperties.tsx
+++ libs/components/mypage/MyProperties.tsx
@@ -1,180 +1,103 @@
 import React, { useState } from 'react';
 import { NextPage } from 'next';
-import { Pagination, Stack, Typography } from '@mui/material';
 import useDeviceDetect from '../../hooks/useDeviceDetect';
-import { PropertyCard } from './PropertyCard';
-import { useMutation, useQuery, useReactiveVar } from '@apollo/client';
+import { Pagination, Stack, Typography } from '@mui/material';
+import PropertyCard from '../property/PropertyCard';
 import { Property } from '../../types/property/property';
-import { AgentPropertiesInquiry } from '../../types/property/property.input';
 import { T } from '../../types/common';
-import { PropertyStatus } from '../../enums/property.enum';
-import { userVar } from '../../../apollo/store';
-import { useRouter } from 'next/router';
-import { UPDATE_PROPERTY } from '../../../apollo/user/mutation';
-import { GET_AGENT_PROPERTIES } from '../../../apollo/user/query';
-import { sweetConfirmAlert, sweetErrorHandling } from '../../sweetAlert';
+import { useMutation, useQuery } from '@apollo/client';
+import { LIKE_TARGET_PROPERTY } from '../../../apollo/user/mutation';
+import { GET_FAVORITES } from '../../../apollo/user/query';
+import { Messages } from '../../config';
+import { sweetMixinErrorAlert } from '../../sweetAlert';
 
-const MyProperties: NextPage = ({ initialInput, ...props }: any) => {
+const MyFavorites: NextPage = () => {
 	const device = useDeviceDetect();
-	const [searchFilter, setSearchFilter] = useState<AgentPropertiesInquiry>(initialInput);
-	const [agentProperties, setAgentProperties] = useState<Property[]>([]);
+	const [myFavorites, setMyFavorites] = useState<Property[]>([]);
 	const [total, setTotal] = useState<number>(0);
-	const user = useReactiveVar(userVar);
-	const router = useRouter();
+	const [searchFavorites, setSearchFavorites] = useState<T>({ page: 1, limit: 6 });
 
 	/** APOLLO REQUESTS **/
-	const [updateProperty] = useMutation(UPDATE_PROPERTY);
+	const [likeTargetProperty] = useMutation(LIKE_TARGET_PROPERTY);
 
 	const {
-		loading: getAgentPropertiesLoading,
-		data: getAgentPropertiesData,
-		error: getAgentPropertiesError,
-		refetch: getAgentPropertiesRefetch,
-	} = useQuery(GET_AGENT_PROPERTIES, {
+		loading: getFavoritesLoading,
+		data: getFavoritesData,
+		error: getFavoritesError,
+		refetch: getFavoritesRefetch,
+	} = useQuery(GET_FAVORITES, {
 		fetchPolicy: 'network-only',
-		variables: { input: searchFilter },
+		variables: { input: searchFavorites },
 		notifyOnNetworkStatusChange: true,
 		onCompleted: (data: T) => {
-			setAgentProperties(data?.getAgentProperties?.list);
-			setTotal(data?.getAgentProperties?.metaCounter[0]?.total ?? 0);
+			setMyFavorites(data?.getFavorites?.list);
+			setTotal(data?.getFavorites?.metaCounter?.[0]?.total || 0);
 		},
 	});
 
 	/** HANDLERS **/
 	const paginationHandler = (e: T, value: number) => {
-		setSearchFilter({ ...searchFilter, page: value });
+		setSearchFavorites({ ...searchFavorites, page: value });
 	};
 
-	const changeStatusHandler = (value: PropertyStatus) => {
-		setSearchFilter({ ...searchFilter, search: { propertyStatus: value } });
-	};
-
-	const deletePropertyHandler = async (id: string) => {
+	const likePropertyHandler = async (user: any, id: string) => {
 		try {
-			if (await sweetConfirmAlert('Are you sure to delete this property?')) {
-				await updateProperty({
-					variables: {
-						input: {
-							_id: id,
-							propertyStatus: 'DELETE',
-						},
-					},
-				});
-				await getAgentPropertiesRefetch({ input: searchFilter });
-			}
+			if (!id) return;
+			if (!user._id) throw new Error(Messages.error2);
+
+			await likeTargetProperty({ variables: { input: id } });
+
+			await getFavoritesRefetch({ input: searchFavorites });
 		} catch (err: any) {
-			await sweetErrorHandling(err);
+			console.log('ERROR, likePropertyHandler:', err.message);
+			sweetMixinErrorAlert(err.message).then();
 		}
 	};
-
-	const updatePropertyHandler = async (status: string, id: string) => {
-		try {
-			if (await sweetConfirmAlert(`Are you sure to change to ${status} status?`)) {
-				await updateProperty({
-					variables: {
-						input: {
-							_id: id,
-							propertyStatus: status,
-						},
-					},
-				});
-				await getAgentPropertiesRefetch({ input: searchFilter });
-			}
-		} catch (err: any) {
-			await sweetErrorHandling(err);
-		}
-	};
-
-	if (user?.memberType !== 'AGENT') {
-		router.back();
-	}
 
 	if (device === 'mobile') {
-		return <div>NESTAR PROPERTIES MOBILE</div>;
+		return <div>NESTAR MY FAVORITES MOBILE</div>;
 	} else {
 		return (
-			<div id="my-property-page">
+			<div id="my-favorites-page">
 				<Stack className="main-title-box">
 					<Stack className="right-box">
-						<Typography className="main-title">My Properties</Typography>
+						<Typography className="main-title">My Favorites</Typography>
 						<Typography className="sub-title">We are glad to see you again!</Typography>
 					</Stack>
 				</Stack>
-				<Stack className="property-list-box">
-					<Stack className="tab-name-box">
-						<Typography
-							onClick={() => changeStatusHandler(PropertyStatus.ACTIVE)}
-							className={searchFilter.search.propertyStatus === 'ACTIVE' ? 'active-tab-name' : 'tab-name'}
-						>
-							On Sale
-						</Typography>
-						<Typography
-							onClick={() => changeStatusHandler(PropertyStatus.SOLD)}
-							className={searchFilter.search.propertyStatus === 'SOLD' ? 'active-tab-name' : 'tab-name'}
-						>
-							On Sold
-						</Typography>
-					</Stack>
-					<Stack className="list-box">
-						<Stack className="listing-title-box">
-							<Typography className="title-text">Listing title</Typography>
-							<Typography className="title-text">Date Published</Typography>
-							<Typography className="title-text">Status</Typography>
-							<Typography className="title-text">View</Typography>
-							{searchFilter.search.propertyStatus === 'ACTIVE' && (
-								<Typography className="title-text">Action</Typography>
-							)}
-						</Stack>
-
-						{agentProperties?.length === 0 ? (
-							<div className={'no-data'}>
-								<img src="/img/icons/icoAlert.svg" alt="" />
-								<p>No Property found!</p>
-							</div>
-						) : (
-							agentProperties.map((property: Property) => {
-								return (
-									<PropertyCard
-										property={property}
-										deletePropertyHandler={deletePropertyHandler}
-										updatePropertyHandler={updatePropertyHandler}
-									/>
-								);
-							})
-						)}
-
-						{agentProperties.length !== 0 && (
-							<Stack className="pagination-config">
-								<Stack className="pagination-box">
-									<Pagination
-										count={Math.ceil(total / searchFilter.limit)}
-										page={searchFilter.page}
-										shape="circular"
-										color="primary"
-										onChange={paginationHandler}
-									/>
-								</Stack>
-								<Stack className="total-result">
-									<Typography>{total} property available</Typography>
-								</Stack>
-							</Stack>
-						)}
-					</Stack>
+				<Stack className="favorites-list-box">
+					{myFavorites?.length ? (
+						myFavorites?.map((property: Property) => {
+							return <PropertyCard property={property} myFavorites={true} likePropertyHandler={likePropertyHandler} />;
+						})
+					) : (
+						<div className={'no-data'}>
+							<img src="/img/icons/icoAlert.svg" alt="" />
+							<p>No Favorites found!</p>
+						</div>
+					)}
 				</Stack>
+				{myFavorites?.length ? (
+					<Stack className="pagination-config">
+						<Stack className="pagination-box">
+							<Pagination
+								count={Math.ceil(total / searchFavorites.limit)}
+								page={searchFavorites.page}
+								shape="circular"
+								color="primary"
+								onChange={paginationHandler}
+							/>
+						</Stack>
+						<Stack className="total-result">
+							<Typography>
+								Total {total} favorite propert{total > 1 ? 'ies' : 'y'}
+							</Typography>
+						</Stack>
+					</Stack>
+				) : null}
 			</div>
 		);
 	}
 };
 
-MyProperties.defaultProps = {
-	initialInput: {
-		page: 1,
-		limit: 5,
-		sort: 'createdAt',
-		search: {
-			propertyStatus: 'ACTIVE',
-		},
-	},
-};
-
-export default MyProperties;
+export default MyFavorites;
